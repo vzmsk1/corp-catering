6f7b8312a2207edf8c5391b94eeeed65abc1d256
Dima <ctaricc@mail.ru>
2023-12-26 22:37:29 +0300

    feat: services section

---
+++ src/js/swiper.js
@@ -0,0 +1,48 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import { rem } from '@js/utils/utils';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/grid';
+
+function initSwiperSettings(initializer, payload) {
+    if (!document.querySelector(`.${initializer}`)) return;
+
+    return {
+        modules: [Navigation, Pagination],
+        speed: 2100,
+        keyboard: {
+            enabled: true
+        },
+        navigation: {
+            nextEl: `.${initializer}__button-next`,
+            prevEl: `.${initializer}__button-prev`
+        },
+        pagination: {
+            el: `.${initializer}__fraction`,
+            type: 'fraction',
+            formatFractionCurrent: (num) => (num > 9 ? num : '0' + num),
+            formatFractionTotal: (num) => (num > 9 ? num : '0' + num)
+        },
+        ...payload
+    };
+}
+
+new Swiper(
+    '.services-swiper',
+    initSwiperSettings('services', {
+        breakpoints: {
+            0: {
+                slidesPerView: 1.08,
+                spaceBetween: rem(1.6)
+            },
+            768: {
+                spaceBetween: rem(2.4),
+                slidesPerView: 2
+            }
+        }
+    })
+);
